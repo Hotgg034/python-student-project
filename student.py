c8e703c2b745ae85c2fec24ac93ac46816bea770
Hotgg034 <ghaithabbasi07@gmail.com>
2026-05-10 17:45:35 +0200

    Fix the get_grade function

--- student.py
+++ student.py
@@ -1,4 +1,13 @@
 def get_grade(score):
+    if score < 0 or score > 100:
+        return "Error: Invalid score"
+    if score >= 90:
+        return "A"
+    if score >= 75:
+        return "B"
+    if score >= 60:
+        return "C"
+    return "F"def get_grade(score):
     if score >= 90:
         return "A"
     return "B"
